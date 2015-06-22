315954090da981f3b8576b27ae218718aadfa949
adiseño.2015 <adiseo.2015@192.168.56.1>
2015-06-22 11:20:28 -0600

    se creo el modelo para el libro

--- src/modelo/libro.java
+++ src/modelo/libro.java
@@ -5,10 +5,74 @@
  */
 package modelo;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
 /**
  *
  * @author adiseño.2015
  */
+@Entity
 public class libro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int Id;
+    private String Nombre;
+    private String ISB;
+    private String Fecha;
+    private String Editorial;
     
+ @ManyToOne
+ private autor Aid;
+
+    public autor getAid() {
+        return Aid;
+    }
+
+    public void setAid(autor Aid) {
+        this.Aid = Aid;
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int Id) {
+        this.Id = Id;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getISB() {
+        return ISB;
+    }
+
+    public void setISB(String ISB) {
+        this.ISB = ISB;
+    }
+
+    public String getFecha() {
+        return Fecha;
+    }
+
+    public void setFecha(String Fecha) {
+        this.Fecha = Fecha;
+    }
+
+    public String getEditorial() {
+        return Editorial;
+    }
+
+    public void setEditorial(String Editorial) {
+        this.Editorial = Editorial;
+    }
 }
