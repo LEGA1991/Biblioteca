803e7e3f34f2ba1582700043614a2ab2fdcf37c3
adiseño.2015 <adiseo.2015@192.168.56.1>
2015-06-22 11:21:40 -0600

    se creo el modelo para el autor

--- src/modelo/autor.java
+++ src/modelo/autor.java
@@ -4,11 +4,33 @@
  * and open the template in the editor.
  */
 package modelo;
-
+import java.io.Serializable;
+import javax.persistence.*;
 /**
  *
  * @author adiseño.2015
  */
+
+@Entity
 public class autor {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int AId;
+    private String Nombre;
+
+    public int getAId() {
+        return AId;
+    }
+
+    public void setAId(int AId) {
+        this.AId = AId;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
 }
